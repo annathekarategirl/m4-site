30f1cffdf208079413771935664fdea4ab9ef077
annathekarategirl <annathekarategirl@gmail.com>
2026-02-02 10:45:31 -0500

    makefunc

---
+++ js02.js
@@ -0,0 +1,21 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Anna Moore
+      Date:   2/2/2026
+
+      Filename: js02.js
+ */
+// set the form's default values
+
+function setupForm() {
+      document.getElementById("photoNum").value=1;
+      document.getElementById("photoHrs").value=2;
+      document.getElementById("makeBook").checked=false;
+      document.getElementById("photoRights").checked=false;
+      document.getElementById("photoDist").value=0;
+
+}
